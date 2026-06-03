0eaf8d170871c7d2e4ca493153f626de87d1ed36
Avgeek001 <269661@learning.gsis.edu.hk>
2026-06-03 15:02:29 +0800

    Added search functionality

--- views/discover.py
+++ views/discover.py
@@ -1,3 +1,18 @@
 import streamlit as st
+from views.home import loadedCharities
 
-'''This is the discover page'''
+
+def searchFunction(query):
+    IDsFoundTitle = []
+    IDsFoundTags = []
+    IDsFoundDesc = []
+    for charity in loadedCharities:
+        for searchTerm in query:
+            if searchTerm in charity.title:
+                IDsFoundTitle.append(charity.ID)
+            elif searchTerm in charity.tags:
+                IDsFoundTags.append(charity.ID)
+            elif searchTerm in charity.description:
+                IDsFoundDesc.append(charity.ID)
+
+    return IDsFoundTitle + IDsFoundTags + IDsFoundDesc
