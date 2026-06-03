25cf3725f5c9318fe84c1624adc879c0ae8de60c
Marcus Tsang <marcuskmTsang@gmail.com>
2026-06-03 15:19:54 +0800

    Search bar basic functionality

--- views/discover.py
+++ views/discover.py
@@ -8,17 +8,14 @@ def searchFunction(query):
     IDsFoundDesc = []
     for charity in loadedCharities:
         for searchTerm in query:
-            if searchTerm in charity.name:
+            if searchTerm in charity.name.lower():
                 IDsFoundTitle.append(charity.ID)
-            elif searchTerm in charity.tags:
+            elif searchTerm in [tag.lower() for tag in charity.tags]:
                 IDsFoundTags.append(charity.ID)
-            elif searchTerm in charity.description:
+            elif searchTerm in charity.description.lower():
                 IDsFoundDesc.append(charity.ID)
 
     return IDsFoundTitle + IDsFoundTags + IDsFoundDesc
-
-
-'''This is the discover page'''
 
 # Search bar
 search_contents = ""
@@ -29,7 +26,10 @@ else:
     st.session_state["search_bar_data"] = ""
 
 searchTerm = st.text_input("Search for charities...", search_contents)
-if searchTerm:
-    charities = searchFunction(searchTerm.split(" "))
-    for c in charities:
-        st.text(c)
+if searchTerm or search_contents:
+    charities = searchFunction(searchTerm.lower().split(" "))
+    if len(charities) > 0:
+        for c in charities:
+            st.text(c)
+    else:
+        st.text("No charities found")
