4fefb404fa85cfcd9ef0f8b35821cef7df802991
Avgeek001 <269661@learning.gsis.edu.hk>
2026-06-12 08:51:40 +0800

    Added initial card functionality for homepage

--- views/home.py
+++ views/home.py
@@ -1,18 +1,36 @@
 import streamlit as st
 import dataManagement
+from dataManagement import truncateString
 
 loadedCharities = dataManagement.loadCharities()
 
 
 with st.container(key="logoBar"):
     st.image(
-<<<<<<< Updated upstream
-        "images/MatchMyCause Text White.svg"
-=======
         "images/MatchMyCause Text White.svg",
-        width="stretch"
->>>>>>> Stashed changes
+        width="content"
     )
+
+
+with st.container(key="trendingCarousel"):
+    st.markdown("Trending Charities")
+
+    htmlCarousel = '<div class="charityFlexRow">'
+
+    for charity in loadedCharities:
+        id = int(charity.ID)
+        htmlCarousel += (
+        f'<div class="charityCard">'
+        f'<span class="cardTag">{charity.category}</span>'
+        f'<h4 class="cardTitle">{charity.name}</h4>'
+        f'<p class="cardDesc">{truncateString(charity.description,100)}</p>'
+        f'<a href="/charity?id={id}" target="_self" class="cardLinkButton">Learn More</a>'
+        f'</div>'
+        )
+
+    htmlCarousel += "</div>"
+    st.markdown(htmlCarousel, unsafe_allow_html=True)
+
 
 
 # Search bar
@@ -23,6 +41,7 @@ with st.container(key="searchBar"):
     if searchTerm:
         st.session_state["searchBarData"] = searchTerm
         st.switch_page("views/discover.py")
+
 
 with st.container(key="aboutUs", height=1000):
     st.write("About us...")
