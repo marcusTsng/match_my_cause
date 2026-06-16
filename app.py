8adb7f47449d291b33714e03b9adfd1c0cab37c8
Avgeek001 <269661@learning.gsis.edu.hk>
2026-06-16 14:43:16 +0800

    Added tab icon

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from dataManagement import injectCSS
 
-st.set_page_config(page_title="MatchMyCause", layout='wide')
+st.set_page_config(page_title="MatchMyCause", layout='wide', page_icon="images/MatchMyCause Icon.svg")
 
 homePage = st.Page("views/home.py", title="Home", default=True)
 discoverPage = st.Page("views/discover.py", title="Discover")
