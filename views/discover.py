9f6a5abacc887a774e02bc9c17fbe87e630e4e3a
Avgeek001 <269661@learning.gsis.edu.hk>
2026-06-16 15:21:06 +0800

    Improved Discover Page

--- views/discover.py
+++ views/discover.py
@@ -6,18 +6,21 @@ loadedCharities = dataManagement.loadCharities()
 
 def searchFunction(query):
     IDsFoundTitle = []
+    IDsFoundCats = []
     IDsFoundTags = []
     IDsFoundDesc = []
     for searched in loadedCharities:
         for searchTerm in query:
             if searchTerm in searched.name.lower():
                 IDsFoundTitle.append(searched.ID)
+            elif searchTerm in searched.category.lower():
+                IDsFoundCats.append(searched.ID)
             elif any(searchTerm in tag for tag in searched.tags):
                 IDsFoundTags.append(searched.ID)
             elif searchTerm in searched.description.lower():
                 IDsFoundDesc.append(searched.ID)
 
-    return IDsFoundTitle + IDsFoundTags + IDsFoundDesc
+    return IDsFoundTitle + IDsFoundCats + IDsFoundTags + IDsFoundDesc
 
 with st.container(key="logoBar"):
     st.image(
@@ -33,13 +36,13 @@ if "searchBarData" in st.session_state:
 else:
     searchContents = ""
 
-with st.container(key="searchContainer", height=150):
-    searchQuery = st.text_input("", searchContents, placeholder="Search names, tags, categories...", key="searchBox")
+with st.container(key="searchContainer", height=140):
+    searchQuery = st.text_input("", searchContents, icon=":material/search:", placeholder="Search names, tags, categories...", key="searchBox")
 
-with st.container(key="searchResults"):
-    if searchQuery or searchContents:
-        charities = searchFunction(searchQuery.lower().split(" "))
-        if len(charities) > 0:
+if searchQuery or searchContents:
+    charities = searchFunction(searchQuery.lower().split(" "))
+    if len(charities) > 0:
+        with st.container(key="searchResults"):
             charitiesToShow = []
             for loadCharity in loadedCharities:
                 if loadCharity.ID in charities:
@@ -50,9 +53,14 @@ with st.container(key="searchResults"):
                 with st.container(key=f"foundCharity{containerID}"):
                     st.write(charity.name)
                 containerID += 1
-        else:
-            st.write(":material/search_off: No charities found")
     else:
-        st.write("Search anything!")
-
-
+        with st.container(key="noResults"):
+            with st.container(key="noResultsBoth"):
+                with st.container(key="noResultsIcon"):
+                    st.write(":material/search_off:")
+                with st.container(key="noResultsMessage"):
+                    st.write("Search anything!")
+else:
+        with st.container(key="noResults"):
+            with st.container(key="noResultsMessage"):
+                st.write("Search anything!")
