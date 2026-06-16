290afd0ab584fefb1abde0419c865f8165dbb97d
Avgeek001 <269661@learning.gsis.edu.hk>
2026-06-16 08:38:19 +0800

    Added initial content to About Us

--- views/home.py
+++ views/home.py
@@ -55,5 +55,39 @@ with st.container(key="searchBarContainer"):
         st.switch_page("views/discover.py")
 
 
-with st.container(key="aboutUs", height=1000):
-    st.write("About us...")
+with st.container(key="aboutUs", border=True):
+    st.title("About Us")
+
+    aboutColumns1 = st.columns([6,4])
+    with aboutColumns1[0]:
+        st.header("Interesting things")
+        st.write("This is something interesting about us")
+    with aboutColumns1[1]:
+        st.space("xxsmall")
+        with st.container(border=True, key="aboutUsImage1"):
+            st.write("This is an image")
+
+    st.divider()
+
+    aboutColumns2 = st.columns([2, 8])
+    with aboutColumns2[0]:
+        st.space("xxsmall")
+        with st.container(border=True, key="aboutUsImage2"):
+            st.write("This is a portrait")
+    with aboutColumns2[1]:
+        st.header("Author 1")
+        st.write("This is something about one author")
+
+
+    st.divider()
+
+    aboutColumns3 = st.columns([8, 2])
+    with aboutColumns3[0]:
+        st.header("Author 2")
+        st.write("This is something about another author")
+    with aboutColumns3[1]:
+        st.space("xxsmall")
+        with st.container(border=True, key="aboutUsImage3"):
+            st.write("This is a portrait")
+
+    st.space("xsmall")
