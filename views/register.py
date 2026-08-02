d5cee62bc4947c921660892a5b6db9d6d250d5f4
Avgeek001 <269661@learning.gsis.edu.hk>
2026-08-03 00:11:01 +0200

    Started working on Register page

--- views/register.py
+++ views/register.py
@@ -1,3 +1,21 @@
 import streamlit as st
 
-'''This is the registration page for charities'''
+with st.container(key="registerPage"):
+    with st.container(key="registerTitleContainer"):
+        st.markdown('<h2 class="registerTitle">Register your own charity</h2>', unsafe_allow_html=True)
+        st.markdown('<h2 class="registerTitleSmall">Register</h2>', unsafe_allow_html=True)
+
+    with st.container(key="formContainer"):
+        with st.form(key="registerCharity", clear_on_submit=True, enter_to_submit=False):
+            formColumns = st.columns(2)
+            with formColumns[0]:
+                with st.container(key="formColumn1"):
+                    newName = st.text_input("Charity Name", placeholder="Tell us your charity's name!")
+                    newCategory = st.pills("Category",
+                                           ["Animals", "Environment", "Education", "Poverty", "Healthcare", "Culture", "Rights", "Other"],
+                                           required=True)
+
+            with formColumns[1]:
+                test2 = st.text_input("Tags")
+
+            submitted = st.form_submit_button("Submit")
